d9be037e09b1cf9f89b725ac0fdcb3c43f44692b
Marwan Rabbâa <marwan.rabbaa@pandacraft.com>
2017-11-24 13:03:58 +0100

    use an online documentation service

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+# General project information
+
+project = u'builder'
+copyright = u'2017, Marwan Rabbâa'
+author = u'Marwan Rabbâa <waghanza@gmail.com>'
+
+
+extensions = []
+
+# Parse docstrings in python source code
+
+extensions.append('sphinx.ext.autodoc')
+
+
+# The root documentation
+# By default extension is rst (this could be changed)
+
+master_doc = 'index'
